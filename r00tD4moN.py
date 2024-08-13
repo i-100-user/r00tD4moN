40a189ea89a0b16d0ef41f6b48d0f3ff417da13e
i-100-user <usermilarbol@gmail.com>
2024-08-14 00:38:35 +0200

    update Libraries

--- r00tD4moN.py
+++ r00tD4moN.py
@@ -1,2 +1,7 @@
 #!/usr/bin/env python3
 # ⬆ shebang de python
+#librerias ⬇;
+import pyfiglet                 # pyfiglet para crear el banner en este caso 'hackpanel' , se instala con "pip install pyfiglet"
+from termcolor import colored   # termcolor para poner colores a los textos , se  instala con "pip install termcolor"
+import argparse                 # crea el penu de ayuda 
+import requests                 # Realiza solicitudes HTTP 
